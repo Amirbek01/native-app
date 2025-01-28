ce89707eef9818e25eb4bc7dd8b53955ee2726e1
Amirbek01 <113281334+Amirbek01@users.noreply.github.com>
2025-01-28 22:24:34 +0500

    начинаем

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  Button, 
+  TextInput,
+  FlatList,
+  Alert
+} from 'react-native';
+import Card from './components/Card';
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const [sortedProducts, setSortedProducts] = useState([]); // Отсортированные продукты
+  const [ascending, setAscending] = useState(true); // Флаг направления сортировки
+  const [category, setCategory] = useState(""); // Категория для фильтрации
+  const [limit, setLimit] = useState(10); // Лимит продуктов
+
+  async function getProducts() {
+    const URL = "https://dummyjson.com/products";
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data.products.slice(0,10);
+  }
+
+  useEffect(() => {
+    async function init() {
+      const products = await getProducts();
+      setProducts(products || []);
+      setSortedProducts(products || []); // Изначально сохраняем без сортировки
+    }
+    
+    init();
+  }, [limit]);
+  const sortProducts = () => {
+    const sorted = [...sortedProducts].sort((a, b) => {
+      if (ascending) {
+        return a.price - b.price ;
+      }
+      else {
+        return b.price - a.price;
+      }
+        
+    });
+    setSortedProducts(sorted);
+    setAscending(!ascending);
+  }
+  const filterProducts = () => { 
+    Alert.prompt("Введите категорию", "", (text) => {
+      setCategory(text);
+      filter(text);
+      const filtered = products.filter((item) => {
+        return item.category === category;
+      });
+      setSortedProducts(filtered);
+    });
+    
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.main}>
+      <Text style={styles.title}>Products</Text>
+      <Button
+        title={`Сортировать по цене (${ascending ? "↑" : "↓"})`}
+        onPress={sortProducts}
+      />
+
+      <Button 
+        title="Фильтровать по категории"
+        onPress={filterProducts}
+      />
+      
+      {
+        products.length > 0 ?
+          <FlatList
+            data={sortedProducts || []}
+            renderItem={({item}) => <Card item={item} />}
+          /> :
+          <Text>Loading</Text>
+      }
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  main : {
+    backgroundColor: "#b1afa9",
+    marginTop: 26,
   },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 10,
+    color: "#333",
+  }
 });
+
+
---
+++ components/Card.js
@@ -0,0 +1,82 @@
+import { View, StyleSheet, Image, Text ,Button} from "react-native";
+
+export default function Card({ item }) {
+    
+    return (
+        <View style={styles.container}>
+            
+                <Image
+                    source={{
+                        uri: item.images ? item.images[0] : null,
+                        height: 200,
+                    }}
+                    resizeMode="contain"
+                />
+            <View
+                style={{
+                    marginTop: 10,
+                    paddingTop: 16,
+                    borderTopWidth: 1,
+                    borderColor: "#e5e5e5",
+                }}
+            >
+                <View style= {{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'  }}>
+                
+                <View>
+                    <Text style={styles.title}>
+                        {item.title}
+                    </Text>
+                    <Text style={styles.category}>
+                        {item.category}
+                    </Text>
+                </View>
+                <Image
+                    source={{
+                        uri: "https://cdn-icons-png.flaticon.com/128/2550/2550288.png",
+                        marginTop: 10,
+                        height: 30,
+                        width: 30,
+                        
+                    }}
+                    resizeMode="contain"
+                />
+                </View>
+
+                <Text style={styles.price}>
+                    {item.price}$
+                </Text>
+
+                <Button title="I'll buy"/>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        width: "100%",
+        padding: 20,
+        borderRadius: 10,
+        marginTop: 20,
+        borderWidth: 1,
+        borderColor: "lightgray"
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+    category: {
+        fontSize: 18,
+        color: "gray",
+        fontWeight: 700
+    },
+    price: {
+        fontSize: 20,
+        color: "green",
+        fontWeight: 700,
+        textAlign: "right",
+        marginBottom: 10
+    }
+});
